b3b91162635dc6642f67a6bed0873e3ef1bb56d2
Manerenecho <mfxpaja@gmail.com>
2019-04-29 19:47:52 +0200

    update - redone Arduino + started sms

--- app/src/main/java/com/maneren/smartcollar/Arduino.java
+++ app/src/main/java/com/maneren/smartcollar/Arduino.java
@@ -2,27 +2,41 @@ package com.maneren.smartcollar;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 
 public class Arduino {
     private UsbManager usbManager;
     private Context context;
     private UsbDevice device;
+    private UsbSerialDevice serialPort;
+    private Listener mListener;
+    private final String ACTION_USB_PERMISSION = "com.hariharan.arduinousb.USB_PERMISSION";
 
-    Arduino(Activity activity){
-        context = activity.getApplicationContext();
-        usbManager = (UsbManager) activity.getSystemService(Context.USB_SERVICE);
+    Arduino(Activity activityArg/*, Runnable broadcastCallbackArg*/){
+        context = activityArg.getApplicationContext();
+        usbManager = (UsbManager) activityArg.getSystemService(Context.USB_SERVICE);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        context.registerReceiver(broadcastReceiver, filter);
         Log.d("USB", "created");
     }
 
@@ -45,15 +59,69 @@ public class Arduino {
         } else Toast.makeText(context, "No USB device detected", Toast.LENGTH_SHORT).show();
     }
 
-    public UsbDevice getDevice (){
-        return device;
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        UsbDeviceConnection connection;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (Objects.requireNonNull(intent.getAction())) {
+                case ACTION_USB_PERMISSION:
+                    boolean granted = Objects.requireNonNull(intent.getExtras()).getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                    if (granted) {
+                        connection = usbManager.openDevice(device);
+                        serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                        if (serialPort != null) {
+                            if (serialPort.open()) {
+                                serialPort.setBaudRate(9600);
+                                serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                                serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                                serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                                serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                                serialPort.read(mCallback);
+                                Toast.makeText(context, "USB reciever connected", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Log.d("SERIAL", "PORT NOT OPEN");
+                            }
+                        } else {
+                            Log.d("SERIAL", "PORT IS NULL");
+                        }
+                    } else {
+                        Log.d("SERIAL", "PERM NOT GRANTED");
+                    }
+                    break;
+                case UsbManager.ACTION_USB_DEVICE_ATTACHED:
+                    connect();
+                    break;
+                case UsbManager.ACTION_USB_DEVICE_DETACHED:
+                    disconnect();
+                    break;
+            }
+        }
+    };
+
+    private UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() {
+        @Override
+        public void onReceivedData(byte[] arg0) {
+            String data = new String(arg0, StandardCharsets.UTF_8).concat("/n");
+            mListener.recieveCallback(data);
+            Log.d("USB", data);
+        }
+    };
+
+    public interface Listener {
+        void recieveCallback(String str);
+    }
+
+    void setListener(Listener listener) {
+        mListener = listener;
     }
 
     void send(UsbSerialDevice serialPort, String msg){
         serialPort.write(msg.getBytes());
     }
 
-    void disconnect(UsbSerialDevice serialPort){
-        serialPort.close();
+    void disconnect(){
+        if (serialPort != null) serialPort.close();
+        context.unregisterReceiver(broadcastReceiver);
     }
 }
--- app/src/main/java/com/maneren/smartcollar/Walk.java
+++ app/src/main/java/com/maneren/smartcollar/Walk.java
@@ -1,101 +1,66 @@
 package com.maneren.smartcollar;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import com.felhr.usbserial.UsbSerialDevice;
-import com.felhr.usbserial.UsbSerialInterface;
-
-import java.nio.charset.StandardCharsets;
 
 public class Walk extends AppCompatActivity {
     Arduino arduino;
     Context context;
-    UsbManager usbManager;
-    UsbSerialDevice serialPort;
-    final String ACTION_USB_PERMISSION = "com.hariharan.arduinousb.USB_PERMISSION";
+    private Walk instance = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        arduino = null;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
         context = this.getApplicationContext();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(ACTION_USB_PERMISSION);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        context.registerReceiver(broadcastReceiver, filter);
         int time = 0;
-        int[] locations = {};
+        String[] locations = {};
+    }
+
+    public void onRecieveCallback(String str){
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
     }
 
     public void useUSB(View view){
-        arduino = new Arduino(this);
+        arduino = new Arduino(instance);
+        arduino.setListener(new Arduino.Listener() {
+            public void recieveCallback(String data) {
+                onRecieveCallback(data);
+            }
+        });
         arduino.connect();
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Finish Walk")
+                .setMessage("Are you sure you want to finish the walk?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
-        arduino.disconnect(serialPort);
-        context.unregisterReceiver(broadcastReceiver);
+        arduino.disconnect();
+        //if (sms)
     }
-    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        UsbDeviceConnection connection;
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            UsbDevice device = arduino.getDevice();
-            switch (intent.getAction()) {
-                case ACTION_USB_PERMISSION:
-                    boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
-                    if (granted) {
-                        connection = usbManager.openDevice(device);
-                        serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
-                        if (serialPort != null) {
-                            if (serialPort.open()) {
-                                serialPort.setBaudRate(9600);
-                                serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
-                                serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
-                                serialPort.setParity(UsbSerialInterface.PARITY_NONE);
-                                serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                                serialPort.read(mCallback);
-                                Toast.makeText(context, "USB reciever connected", Toast.LENGTH_SHORT).show();
 
-                            } else {
-                                Log.d("SERIAL", "PORT NOT OPEN");
-                            }
-                        } else {
-                            Log.d("SERIAL", "PORT IS NULL");
-                        }
-                    } else {
-                        Log.d("SERIAL", "PERM NOT GRANTED");
-                    }
-                    break;
-                case UsbManager.ACTION_USB_DEVICE_ATTACHED:
-                    arduino.connect();
-                    break;
-                case UsbManager.ACTION_USB_DEVICE_DETACHED:
-                    arduino.disconnect(serialPort);
-                    break;
-            }
-        }
-    };
 
-    private UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() {
-        @Override
-        public void onReceivedData(byte[] arg0) {
-            String data = new String(arg0, StandardCharsets.UTF_8).concat("/n");
-            Log.d("USB", data);
-        }
-    };
 }
