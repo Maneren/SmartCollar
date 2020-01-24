0d349974160116cb9208268a82c78001429d84c6
Maneren <mfxpaja@gmail.com>
2020-01-24 18:28:54 +0100

    icon for v fence

---
+++ product2/src/main/java/com/maneren/product2/Timer.java
@@ -0,0 +1,69 @@
+package com.maneren.smartcollar;
+
+import android.os.Handler;
+
+import java.text.DecimalFormat;
+
+class Timer {
+
+    private long startTime = 0;
+    private Listener mListener;
+
+    private final Handler timerHandler = new Handler();
+    private final Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int hours = seconds / 3600;
+            seconds %= 3600;
+            int minutes = seconds / 60;
+            seconds %= 60;
+
+            DecimalFormat df = new DecimalFormat("00");
+
+            String time = ""
+                    .concat(df.format(hours))
+                    .concat(":")
+                    .concat(df.format(minutes))
+                    .concat(":")
+                    .concat(df.format(seconds));
+
+            mListener.recieveCallback(time);
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
+    Timer() {
+
+    }
+
+
+
+    void start(){
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+    void pause(){
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
+    void resume(){
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+    void stop() {
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
+    public interface Listener {
+        void recieveCallback(String str);
+    }
+
+    void setListener(Timer.Listener listener) {
+        mListener = listener;
+    }
+}
